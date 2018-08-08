7d375212a2b72e766fcaac423fe273e2c88d40d2
Fathima1997 <fathimareeza9@yahoo.com>
2018-08-08 23:05:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays(String) {
 return "Happy holidays!";// write your code below!
 }
 
-function happyHolidaysTo(String) {
-  var name = "Fathima"
+function say(Greeting, Name) {
+
   return "Happy holidays,$(name)!"
 }
